bf4a40fcf5086d79436ff653f39aaf8774bd66c9
DevExpressExampleBot <fakeEmail>
2019-02-16 02:48:58 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication1/Form1.vb
+++ VB/WindowsApplication1/Form1.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports DevExpress.XtraGrid
+﻿Imports DevExpress.XtraGrid
 Imports DevExpress.XtraGrid.Views.Grid
 Imports System
 Imports System.Data
@@ -7,35 +6,36 @@ Imports System.Diagnostics
 Imports System.Windows.Forms
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Public Function GetData() As DataTable
-			Dim dt = New DataTable()
-			dt.Columns.Add("Col1", GetType(String))
-			dt.Columns.Add("Col2", GetType(DateTime))
-			dt.Columns.Add("Col3", GetType(Int32))
-			For i As Integer = 0 To 10
-				dt.Rows.Add(New Object() { i, DateTime.Today, i })
-			Next i
-			Return dt
-		End Function
-		Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles simpleButton1.Click
-			Dim grid As New GridControl()
-			grid.BindingContext = New BindingContext()
-			Dim gridview As New GridView()
-			grid.MainView = gridview
-			gridview.GridControl = grid
+    Partial Public Class Form1
+        Inherits Form
 
-			grid.DataSource = GetData()
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+        Public Function GetData() As DataTable
+            Dim dt = New DataTable()
+            dt.Columns.Add("Col1", GetType(String))
+            dt.Columns.Add("Col2", GetType(Date))
+            dt.Columns.Add("Col3", GetType(Int32))
+            For i As Integer = 0 To 10
+                dt.Rows.Add(New Object() { i, Date.Today, i })
+            Next i
+            Return dt
+        End Function
+        Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles simpleButton1.Click
+            Dim grid As New GridControl()
+            grid.BindingContext = New BindingContext()
+            Dim gridview As New GridView()
+            grid.MainView = gridview
+            gridview.GridControl = grid
 
-			grid.ForceInitialize()
-			gridview.PopulateColumns()
+            grid.DataSource = GetData()
 
-			grid.ExportToXls("1.xls")
-			Process.Start("1.xls")
-		End Sub
-	End Class
+            grid.ForceInitialize()
+            gridview.PopulateColumns()
+
+            grid.ExportToXls("1.xls")
+            Process.Start("1.xls")
+        End Sub
+    End Class
 End Namespace
